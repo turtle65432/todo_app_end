24cfc3e7e51a3b76bb5af3e92d4ae9001219b928
abdoukhadr seck <abdouseck98@gmail.com>
2017-11-18 21:54:52 +0000

    params

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,5 +2,9 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        @todo_description='Make a curriculum'
+        @todo_pomodoro_estimate=4
+            
     end
+    
 end
