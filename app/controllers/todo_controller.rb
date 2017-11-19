d9724bbc7b77310ceaeac4fd269e7cbbf9e51f02
abdoukhadr seck <abdouseck98@gmail.com>
2017-11-19 17:54:58 +0000

    sudo

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,7 +4,14 @@ class TodoController < ApplicationController
     def show
         @todo_description='Make a curriculum'
         @todo_pomodoro_estimate=4
-            
+        todo_id=params[:id]
+        if todo_id=='1'
+            @todo_description="make the curriculum"
+            @todo_pomodoros=4
+        elsif todo_id=='2'
+            @todo_description="Turtles"
+            @todo_pomodoros=2
+        end   
     end
     
 end
